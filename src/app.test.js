7283333ecc03f4b38b8f6cb00d4543c6bf91bde1
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-17 23:53:27 -0400

    added mocha chai unit tests

---
+++ src/app.test.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+var qbRankList = require('./app');
+
+describe('nfl-qb-rank', function() {
+
+    // testing listAll
+    describe('listAll', function() {
+        it('should be an array of objects', function() {
+
+            expect(qbRankList.listAll).to.satisfy(isArrayOfObjects);
+
+            function isArrayOfObjects(array) {
+                return array.every(function(item) {
+                    return typeof item === 'object';
+                });
+            }
+
+        });
+    });
+
+    // testing random()
+    describe('random', function() {
+      it('should return a random item from the from qbRankList.listAll', function() {
+
+        var randomItem = qbRankList.random();
+
+        expect(qbRankList.listAll).to.include(randomItem);
+
+      });
+    });
+});
