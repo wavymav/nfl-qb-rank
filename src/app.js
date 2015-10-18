1ce79a929a647a487edb368513aeec1f834cbbc0
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-17 21:43:07 -0400

    adding the library

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-var qbRankList = require('./qb-rank-list');
+var uniqueRandomArr = require('unique-random-array');
+var qbRankList = require('./qb-rank-list.json');
 
 module.exports = {
+    // lists all qb {}'s
     listAll: qbRankList,
+
+    // returns a random player {}
+    random: uniqueRandomArr(qbRankList)
 };
