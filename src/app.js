7d033cedb9531af11617b7a6ebf2520a4b17ecd7
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-18 01:47:43 -0400

    feat(random): Now able to get an array of random passer objects
    By passing a number to the random method you will get an array of player objects & the array length
    will match the passed in number

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 var uniqueRandomArr = require('unique-random-array');
 var qbRankList = require('./qb-rank-list.json');
 
+var getRandomItem = uniqueRandomArr(qbRankList);
+
+var random = function(number) {
+
+    if (number === undefined) {
+
+        return getRandomItem();
+
+    } else {
+
+        var randomItems = [];
+
+        for (var i = 0; i < number; i++) {
+            randomItems.push(getRandomItem());
+        }
+
+        return randomItems;
+
+    }
+};
+
 module.exports = {
+    
     // lists all qb {}'s
     listAll: qbRankList,
 
     // returns a random player {}
-    random: uniqueRandomArr(qbRankList)
+    random: random
+
 };
--- src/app.test.js
+++ src/app.test.js
@@ -5,6 +5,7 @@ describe('nfl-qb-rank', function() {
 
     // testing listAll
     describe('listAll', function() {
+
         it('should be an array of objects', function() {
 
             var isArrayOfObjects = function(array) {
@@ -15,19 +16,32 @@ describe('nfl-qb-rank', function() {
 
             expect(qbRankList.listAll).to.satisfy(isArrayOfObjects);
 
-
-
         });
+
     });
 
     // testing random()
     describe('random', function() {
+
         it('should return a random item from the from qbRankList.listAll', function() {
 
             var randomItem = qbRankList.random();
 
             expect(qbRankList.listAll).to.include(randomItem);
 
-      });
+        });
+
+        it('should return an array of random items if passed a number', function() {
+
+            var randomItems = qbRankList.random(5);
+
+            expect(randomItems).to.have.length(5);
+
+            randomItems.forEach(function(item) {
+                expect(qbRankList.listAll).to.include(item);
+            });
+
+        });
+
     });
 });
